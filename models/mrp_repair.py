93c2420d288c4293eba6d1df35890ea0944a15f0
monik43 <monimons666@gmail.com>
2021-11-08 10:55:50 +0100

    warranty field in mrp repair line

--- models/mrp_repair.py
+++ models/mrp_repair.py
@@ -5,4 +5,4 @@ from odoo import api, fields, models, _
 class mrp_repair(models.Model):
     _inherit = "mrp.repair.line"
 
-    warranty = fields.Selection([("iw", "IW"), ("oow", "OOW")], "Garantía")
+    warranty = fields.Selection([("iw", "IW"), ("oow", "OOW"), ("na", "-")], "Garantía")
