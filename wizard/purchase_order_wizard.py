6757eb33d84757a01df7e147cd45534d2209e0da
monik43 <monimons666@gmail.com>
2021-11-09 10:24:45 +0100

    test seller_id

--- wizard/purchase_order_wizard.py
+++ wizard/purchase_order_wizard.py
@@ -109,8 +109,7 @@ class Getmrprepairdata(models.TransientModel):
 
     new_order_line_id = fields.Many2one("create.purchaseorder_mrp")
 
-    
-    seller_id = fields.Many2one('res.partner', compute="_compute_seller_id")
+    seller_id = fields.Many2one("res.partner", compute="_compute_seller_id")
 
     product_id = fields.Many2one("product.product", string="Product", required=True)
     name = fields.Char(string="Description")
@@ -133,18 +132,17 @@ class Getmrprepairdata(models.TransientModel):
     warranty = fields.Selection(
         [("iw", "IW"), ("oow", "OOW"), ("na", "-")], "Garantía", default="na"
     )
-    
+
     @api.depends("product_qty", "price_unit")
     def _compute_total(self):
         for record in self:
             record.product_subtotal = record.product_qty * record.price_unit
 
-    @api.depends('product_id')
+    @api.depends("product_id")
     def _compute_seller_id(self):
         for rec in self:
-            for line in rec.product_id.seller_ids:
-                print(f"""
-                    seller_id id -> {line.name.id}
-                    seller_id name -> {line.name.name}
-                    """)
-                #if line.name.id in (12300,10198):
+            print(f"""
+                product id -> {rec.product_id.id}
+                product name -> {rec.product_id.name}
+                product, seller_ids -> {rec.product_id.seller_ids}
+                """)
