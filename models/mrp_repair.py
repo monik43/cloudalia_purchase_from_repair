165734ac699236eb1c35300ac4ed7d0121906034
monik433 <monimons666@gmail.com>
2021-11-18 10:55:52 +0100

    cambio almacen, añadir por defecto

--- models/mrp_repair.py
+++ models/mrp_repair.py
@@ -8,3 +8,18 @@ class mrp_repair(models.Model):
     warranty = fields.Selection(
         [("iw", "IW"), ("oow", "OOW"), ("bad", "BAD")], "Garantía", required=True
     )
+    type = fields.Selection(
+        [("add", "Add"), ("remove", "Remove")], "Type", required=True, default="add"
+    )
+
+    @api.onchange("warranty")
+    def onchange_warranty(self):
+        for line in self:
+            ASUS_ID = 33
+            SAT_ID = 66
+            if line.warranty == "iw":
+                line.location_id = self.env["stock.location"].browse(ASUS_ID)
+            elif line.warranty == "oow":
+                line.location_id = self.env["stock.location"].browse(SAT_ID)
+            elif line.warranty == "bad":
+                line.location_id = self.env["stock.location"].browse(4)
