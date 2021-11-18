2062645df476a1dc438404c9a99f380b6ea95d9e
monik433 <monimons666@gmail.com>
2021-11-18 10:30:52 +0100

    warranty change

--- models/mrp_repair.py
+++ models/mrp_repair.py
@@ -6,5 +6,5 @@ class mrp_repair(models.Model):
     _inherit = "mrp.repair.line"
 
     warranty = fields.Selection(
-        [("iw", "IW"), ("oow", "OOW"), ("na", "-")], "Garantía", default="na"
+        [("iw", "IW"), ("oow", "OOW"), ("bad", "BAD")], "Garantía", required=True
     )
